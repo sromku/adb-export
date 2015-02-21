59a4f3737b7be99b96a4eeda96c60ef81e6c903f
Roman Kushnarenko <sromku@gmail.com>
2015-02-21 13:39:01 +0200

    Formatting and cleaning the script. Added first spinner.

--- adb-export.sh
+++ adb-export.sh
@@ -1,105 +1,146 @@
 #!/usr/bin/env bash
 
-# start from 
-#aa=(adb shell content query --uri content://com.android.calendar/events --where "dtstart > '1424131251000' and dtend < '1424217651000'" ))
 
-#aa=(adb shell content query --uri content://com.android.calendar/events --where "allDay = 1 and dtstart > '1424044851000' and dtend < '1424304051000'" ))
+# steps:
+# 1. query via adb and export the output to file
+# 2. import the raw data
+# 3. break to rows
+# 4. write each transformed row to csv
 
-# adb shell "
-# content query --uri content://com.android.calendar/events
-# " > res.txt
 
-#echo `adb shell`
-#echo `content query --uri content://com.android.calendar/events --where "dtstart > '1424131251000' and dtend < '1424217651000'"`
-
+# ============ GENERAL PARAMS =============
 DEBUG=true
+REPLACE_VALUE_COMMAS_TO=" "
+
+EVENTS_URI="content://com.android.calendar/events"
+
+
+
+
+# ========== CREATE WORKING DIR ===========
 outputFileName="result.csv"
 
 # clean the file
 > $outputFileName
 
+
+file="raw_query.txt"
+
+
+# ============== FUNCTIONS ================
+
+# find the first poistion of substring 
+# params: 
+#	- string we look in
+#	- string we look for
+# output:
+#	- position of the first occurance
 strindex() { 
-  x="${1%%$2*}"
-  [[ $x = $1 ]] && echo -1 || echo ${#x}
+	x="${1%%$2*}"
+	[[ $x = $1 ]] && echo -1 || echo ${#x}
 }
 
-# function to convert android row to csv row
-# row example:
-#	Row: 0 sync_data10=NULL, cal_sync6=NULL, rrule=NULL, sync_data6=NULL, cal_sync8=1423664730492, eventTimezone=Asia/Jerusalem, _sync_id=pcr4ma0g31n6m0e76c6cno4i10_20150217T080000Z, hasAttendeeData=1, customAppPackage=NULL, originalInstanceTime=1424160000000, sync_data2=NULL, allowedReminders=0,1,2, calendar_timezone=Asia/Jerusalem, uid2445=NULL, dirty=0, originalAllDay=0, cal_sync10=NULL, calendar_color=-6306073, exrule=NULL, cal_sync3=NULL, lastDate=1424161800000, canOrganizerRespond=1, guestsCanSeeGuests=1, rdate=NULL, account_type=com.google, eventEndTimezone=NULL, selfAttendeeStatus=0, cal_sync2=NULL, mutators=NULL, exdate=NULL, hasExtendedProperties=0, calendar_color_index=15, organizer=nir@everything.me, eventColor_index=NULL, sync_data9=0, cal_sync5=0, eventColor=NULL, cal_sync4=1, availability=0, ownerAccount=nir@everything.me, dtstart=1424160000000, lastSynced=0, duration=NULL, accessLevel=0, maxReminders=5, displayColor=-6306073, allDay=0, dtend=1424161800000, eventStatus=1, sync_data3=NULL, sync_data4="2835027570380000", original_id=NULL, _id=1215, guestsCanModify=0, calendar_access_level=100, customAppUri=NULL, calendar_displayName=nir@everything.me, sync_data5=2014-12-02T09:49:45.000Z, guestsCanInviteOthers=1, sync_data8=NULL, cal_sync9=NULL, original_sync_id=pcr4ma0g31n6m0e76c6cno4i10, cal_sync1=nir@everything.me, cal_sync7=NULL, canModifyTimeZone=1, visible=1, allowedAttendeeTypes=0,1,2, sync_data1=pcr4ma0g31n6m0e76c6cno4i10@google.com, allowedAvailability=0,1, description=, title=, calendar_id=11, sync_data7=NULL, deleted=0, eventLocation=, account_name=roman@everything.me, hasAlarm=0, isOrganizer=1
+spinner() {
+	echo $1
+    local pid=$!
+    local delay=0.1
+    local spinstr='|/-\'
+    while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+        local temp=${spinstr#?}
+        printf " [%c]  " "$spinstr"
+        local spinstr=$temp${spinstr%"$temp"}
+        sleep $delay
+        printf "\b\b\b\b\b\b"
+    done
+    printf "    \b\b\b\b"
+}
+
+# convert android row to csv row
+# params:
+#	- android exported raw row
+#	- boolean flag to tell if columns should be printed (in fact, this is valid for first row only)
 toCsv() {
-	# $1 - the row (string)
-	# $2 - print columns (boolean)
 
+	# columns of row
 	columns=()
-	values=()
 
-	showColumns=$2
-	i=0
+	# values of row
+	values=()
 
 	# the key value
 	keyValue=""
+
+	# helper flag to aggregate items into keyvalue
 	next=false
 
-	for item in $1
+	# count number of fields
+	i=0
+
+	# remove all "," and convert 
+	row=$1
+	# row=$(tr "," "$REPLACE_VALUE_COMMAS_TO" <<< "$1")
+
+	for item in $row
 	do
 		# skip the first two (they will be Row\n<number>)
 		if [ $i -eq 0 -o $i -eq 1 ]; then
+			if [ $i -eq 1 ]; then
+				echo $item
+			fi
 			let "i+=1"
 			continue
 		fi
 
-		# if [ $2 = true ]; then 
-		# 	echo $item
-		# fi
-
-		next=false
-
-		# last char
-		concat="${keyValue}${item}"
-		# concat=$(printf "%s %s" "$keyValue $item")
-		keyValue=$(printf "%s" "$concat")
-		#$(printf "%s %s" "$keyValue" "$item")
-		# if [ $2 = true ]; then 
-		# 	echo $keyValue
-		# fi
+		# if last char is "," then we have valid key=value
+		keyValue=$(printf "%s" "${keyValue}${item}")
 		lastChar="${item: -1}"
 		if [ $lastChar = "," ]; then
 			let "i+=1"
 			next=true 
 		fi
 
+		# if we have valid key value, then export to CSV
 		if [ $next = true ]; then
-			# if [ $2 = true ]; then 
-			# 	echo $keyValue
-			# 	echo "---"
-			# fi
 
+			# fetch the column & value
 			position=$(strindex "$keyValue" "=")
+			if [ $position -eq -1 ]; then
+				# yes, it may happen and we will need to take this one and update the previous value
+
+				let "i-=1"
+				# echo $keyValue
+				# values[$i]="${values[i]}$keyValue"
+				# echo ${values[i]}
+				continue
+			fi
+
 			column=${keyValue:0:position}
 			value=${keyValue:position+1}
 
-			# DO IT ONLY FOR CSV
-			# except last char and replace all commas in value to space
+			# -- DO IT ONLY FOR CSV --
+			# except last char, replace all commas to make the CSV to be valid
 			value="${value%?}"
-			value=$(tr ',' ' ' <<< "$value")
+			value=$(tr "," "$REPLACE_VALUE_COMMAS_TO" <<< "$value")
 			value="${value},"
+			# --^-----------------^--
 
+			# aggregate columns and values
 			columns+=($column)
 			values+=($value)
 
+			# go for next field
 			next=false
 			keyValue=""
 		fi
 		
 	done
 
-	# add last one
+	# add last field 
 	position=$(strindex "$keyValue" "=")
 	column=${keyValue:0:position}
 	value=${keyValue:(position+1)}
 	columns+=($column)
 	values+=($value)
-	let "i+=1"
 
 	# check if we need to print columns
 	if [ $2 == true ]; then
@@ -120,14 +161,35 @@ toCsv() {
 
 }
 
-# some dificulties:
-# - it could be new lines in the middle and '+=' doesn't work with just $line. we should use printf string format
-# - we can't match by '=' or by ',' since these chars could be part of the field value itself
-# - i don't want to match with predefined field names, but keep it dynamic
-# - new lines in values
+containsElement () {
+  local e
+  for e in "${@:2}"; do [[ "$e" == "$1" ]] && return 0; done
+  return 1
+}
 
-file="raw_query.txt"
-seperator="Row:"
+# ========== RUN ADB CONENT CMD ===========
+
+dbquery() {
+	adb shell "
+	content query --uri content://com.android.calendar/events
+	" > $file
+}
+
+# start from 
+#aa=(adb shell content query --uri content://com.android.calendar/events --where "dtstart > '1424131251000' and dtend < '1424217651000'" ))
+
+#aa=(adb shell content query --uri content://com.android.calendar/events --where "allDay = 1 and dtstart > '1424044851000' and dtend < '1424304051000'" ))
+
+
+
+#echo `adb shell`
+#echo `content query --uri content://com.android.calendar/events --where "dtstart > '1424131251000' and dtend < '1424217651000'"`
+
+(dbquery) &
+spinner "Querying DB"
+
+
+# ============ EXPORT TO CSV ==============
 
 # prepare all rows in this array
 declare -a rows
@@ -143,7 +205,7 @@ do
 	# - the line that doesn't start with 'Row:' is something that contains fields and data whuch are belong to previous row
 
 	# the position of 'Row:' in the line
-    index=$(strindex "$line" "$seperator")
+    index=$(strindex "$line" "Row:")
     if [ $index -eq 0 ]; then
     	# this is a new row
     	let "count+=1"
@@ -152,31 +214,16 @@ do
     	# this is still the previous row
     	rows[$count]+=$(printf "%s" "$line")
     fi
-done < "$file"
+done < "$file" 
+# spinner "Reading rows"
 
-# print all rows
-if [ $DEBUG = true ]; then
-	for i in "${!rows[@]}"
-	do
-		echo ${rows[i]}
-	done
-	echo "===================="
-fi
-
-# time to fetch all playing field names :)
-# the way to do it:
-# - we take first row
-# - iterate over items and check if item ends with ','
 for i in "${!rows[@]}"
 do
+	# for each row export to CSV
 	toCsv "${rows[i]}" $(if [ $i -eq 0 ]; then echo "true"; else echo "false"; fi)
-done
-
-# declare -a columns
-# firstRow=${rows[5]}
-# for item in $firstRow; do
-# 	echo $item
-# done 
+	
+done 
+# spinner "Writing to CSV"
 
 # export results
 echo "Result:"
